39b7f023e2f23dbcccba17d4a14c8424869a0fa5
Rafaela Augusto S <rafaela.augusto@somei.com.vc>
2022-04-17 18:20:55 -0300

    first commit

---
+++ app/src/main/java/br/com/mobile/thenulos/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.mobile.thenulos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+    }
+
+}
