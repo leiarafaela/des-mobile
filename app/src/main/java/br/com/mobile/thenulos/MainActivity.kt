b1961ec973ac3629b00b99aaa3a88f2eb1b1e5bf
Rafaela Augusto <rafaela20augusto@gmail.com>
2022-04-20 09:19:27 -0300

    screen changed

--- app/src/main/java/br/com/mobile/thenulos/MainActivity.kt
+++ app/src/main/java/br/com/mobile/thenulos/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.mobile.thenulos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -10,12 +11,32 @@ class MainActivity : DebugActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        button_login.setOnClickListener{
+        fun onClickLogin() {
             val valorUser = input_name.text.toString()
             val valorPass = input_password.text.toString()
-            Toast.makeText(this, "Autenticado",Toast.LENGTH_LONG).show()
+            //Toast.makeText(context, "$valorUsuario : $valorSenha", Toast.LENGTH_LONG).show()
 
+            if (valorUser == "aluno" && valorPass == "impacta") {
+                // criar intent
+                val intent = Intent(this, HomeScreenActivity::class.java)
+                // colocar parâmetros (opcional)
+                val params = Bundle()
+                params.putString("nome", input_name.text.toString())
+                intent.putExtras(params)
+
+                // enviar parâmetros simplificado
+                //intent.putExtra("numero", 10)
+
+                // fazer a chamada
+                //startActivity(intent)
+
+                // fazer a chamada esperando resultado
+                startActivityForResult(intent, 1)
+            } else {
+                Toast.makeText(this, "Usuário ou senha incorretos", Toast.LENGTH_LONG).show()
+            }
         }
+        button_login.setOnClickListener{onClickLogin()}
 
     }
 
