26d7be1a7d8a1af4b4f120c6bf868bb908af5d5f
Gustavo Valle <gustavofdovalle@gmail.com>
2022-04-20 22:00:07 -0300

    finalizacao do projeto

--- app/src/main/java/br/com/mobile/thenulos/HomeScreenActivity.kt
+++ app/src/main/java/br/com/mobile/thenulos/HomeScreenActivity.kt
@@ -2,9 +2,16 @@ package br.com.mobile.thenulos
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import kotlinx.android.synthetic.main.activity_home_screen.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.login.*
 
 class HomeScreenActivity : DebugActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +24,22 @@ class HomeScreenActivity : DebugActivity() {
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
+        (menu?.findItem(R.id.action_buscar)?.actionView as SearchView).setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // ação enquanto está digitando
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // ação  quando terminou de buscar e enviou
+                return true
+            }
+
+        })
+        return true
+
+//        return super.onCreateOptionsMenu(menu)
 
     }
 
@@ -25,18 +47,25 @@ class HomeScreenActivity : DebugActivity() {
         val id = item.itemId
 
         if (id == R.id.action_buscar){
-            Toast.makeText(this, "Buscar", Toast.LENGTH_LONG).show()
+
+            Toast.makeText(this,  " Buscar", Toast.LENGTH_LONG).show()
+
 
         } else if (id == R.id.action_atualizar){
-            Toast.makeText(this, "Atualizar", Toast.LENGTH_LONG).show()
-
+            Toast.makeText(this, "Atualizar", Toast.LENGTH_SHORT).show()
+            progressAtualizar.visibility = View.VISIBLE
+            Handler(Looper.getMainLooper()).postDelayed(
+                {
+                    progressAtualizar.visibility = View.GONE
+                }, 10000
+            )
         } else if (id == R.id.action_adicionar){
-            Toast.makeText(this, "Adicionar", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Adicionar", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
 
         } else if (id == R.id.action_sair){
-            Toast.makeText(this, "Sair", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Sair", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
 
--- app/src/main/java/br/com/mobile/thenulos/RegisterActivity.kt
+++ app/src/main/java/br/com/mobile/thenulos/RegisterActivity.kt
@@ -3,9 +3,15 @@ package br.com.mobile.thenulos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home_screen.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.login.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +32,22 @@ class RegisterActivity : AppCompatActivity() {
 
         if (id == android.R.id.home){
             finish()
+        } else if (id == R.id.action_atualizar){
+            Toast.makeText(this, "Atualizar", Toast.LENGTH_SHORT).show()
+            progressAtualizar.visibility = View.VISIBLE
+            Handler(Looper.getMainLooper()).postDelayed(
+                {
+                    progressAtualizar.visibility = View.GONE
+                }, 10000
+            )
+        }
+        else if (id == R.id.action_buscar){
+
+            Toast.makeText(this, " Buscar", Toast.LENGTH_LONG).show()
 
         }
         else if (id == R.id.action_sair) {
-            Toast.makeText(this, "Sair", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Sair", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
 
